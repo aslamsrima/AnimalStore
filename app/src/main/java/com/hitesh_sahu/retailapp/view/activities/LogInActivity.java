5a2b864231d5817155015b051e15fabf0c4465b7
ETERNUS\aslam.shrimali <aslam.shrimali@eternussolutions.com>
2018-05-15 15:46:31 +0100

    Sell Activity Functionality Added

--- app/src/main/java/com/hitesh_sahu/retailapp/view/activities/LogInActivity.java
+++ app/src/main/java/com/hitesh_sahu/retailapp/view/activities/LogInActivity.java
@@ -9,5 +9,6 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frag_login);
+
     }
 }
--- app/src/main/java/com/hitesh_sahu/retailapp/view/activities/SellActivity.java
+++ app/src/main/java/com/hitesh_sahu/retailapp/view/activities/SellActivity.java
@@ -1,13 +1,78 @@
 package com.hitesh_sahu.retailapp.view.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import com.hitesh_sahu.retailapp.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SellActivity extends AppCompatActivity{
+    private RadioButton male,female;
+    private EditText breed, milkRec,Price,Age;
+    private Spinner category, type;
+    ArrayAdapter<String> dataAdapter,petAdapter;
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frag_sell);
+        category=(Spinner)findViewById(R.id.category);
+        type=(Spinner)findViewById(R.id.type);
+        
+        type.setVisibility(View.GONE);
+        List<String> animalList = new ArrayList<String>();
+        animalList.add("Animal");
+        animalList.add("Animal Accesories");
+        animalList.add("Animal Food");
+        animalList.add("Animal Medicine");
+
+        List<String> petList = new ArrayList<String>();
+        petList.add("Pet");
+        petList.add("Pet Accesories");
+        petList.add("Pet Food");
+        petList.add("Pet Medicine");
+        /* Assign the name array to that adapter and
+        also choose a simple layout for the list items */
+        dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, animalList);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        petAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, petList);
+        petAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+
+        category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+                if(position==1){
+                    type.setAdapter(dataAdapter);
+                    type.setVisibility(View.VISIBLE);
+                }else if(position==2){
+                    type.setAdapter(petAdapter);
+                    type.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
 
     }
 
